e05df77f1156b3ce38bf2184b957c30f6bd6fe55
Lucy-sins <Arkkeies967@gmail.com>
2026-03-14 14:47:45 +0530

    Java Test 01

--- Test01.java
+++ Test01.java
@@ -1,29 +1,41 @@
 package java_test_0001_12101;
 
 public class Test01 {
-    private static final int PATTERN_SIZE = 5;
-
     public static void main(String[] args) {
-        for (int i = 1; i <= PATTERN_SIZE; i++) {
-            printPatternRow(PATTERN_SIZE - i, 2 * i - 1);
-        }
+        int n = 5;
 
-        for (int i = PATTERN_SIZE - 1; i >= 1; i--) {
-            printPatternRow(PATTERN_SIZE - i, 2 * i - 1);
-        }
-    }
-
-    private static void printPatternRow(int spaces, int stars) {
-        for (int j = 0; j < spaces; j++) {
-            System.out.print(" ");
-        }
-        for (int j = 1; j <= stars; j++) {
-            if (j == 1 || j == stars) {
-                System.out.print("*");
-            } else {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
+            
+            System.out.print("*");
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            
+            System.out.println();
         }
-        System.out.println();
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            System.out.print("*");
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
     }
 }
